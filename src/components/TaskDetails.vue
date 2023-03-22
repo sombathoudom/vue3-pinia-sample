deb016cea28353e3f88ff0b4466aea2bec57dddc
sombathoudom.chheng <sombathoudom.chheng@cogetel.com.kh>
2023-03-22 14:18:47 +0700

    :sparkles:

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -1,18 +1,24 @@
 <template>
-     <div class="task">
-        <h3>{{ task.title }}</h3>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
     <div class="icons">
-      <i class="material-icons"  @click=" taskStore.removeTask(task.id)">delete</i>
-      <i class="material-icons" :class="{active: task.isFav}" @click="taskStore.toggleFav(task.id)">favorite</i>
+      <i class="material-icons" @click="taskStore.removeTask(task.id)"
+        >delete</i
+      >
+      <i
+        class="material-icons"
+        :class="{ active: task.isFav }"
+        @click="taskStore.toggleFav(task.id)"
+        >favorite</i
+      >
     </div>
   </div>
 </template>
 
 <script setup>
-    import { useTaskStore } from '../stores/TaskStore';
-    const props = defineProps({
-            task: Object
-    });
-    const taskStore = useTaskStore();
- 
+import { useTaskStore } from "../stores/TaskStore";
+const props = defineProps({
+  task: Object,
+});
+const taskStore = useTaskStore();
 </script>
